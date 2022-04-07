2a7bbb0c374bb51dc56c327b4e2f90e324bbe1be
Claudio Camba <claudio.a.jose@gmail.com>
2022-04-07 18:19:52 +0100

    Add solution for TOP Foundation/ Javascript Basic/ Fundational Part 4 - 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+    let numTotal = 0,
+        numParameters = [num1, num2];
 
+    for (const num of numParameters) {
+        if (typeof num !== 'number' || num < 0) {
+            numTotal = 'ERROR'
+        }
+    }
+
+    if (num1 < num2 && numTotal !== 'ERROR') {
+        for (let i = num1; i <= num2; i++) { numTotal += i; }
+    } else {
+        for (let i = num2; i <= num1; i++) { numTotal += i; }
+    }
+
+    return numTotal;
 };
 
 // Do not edit below this line
