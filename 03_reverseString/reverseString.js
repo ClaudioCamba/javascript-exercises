d7ba6fb5124d38387ece6b038f201256c430aeca
Claudio Camba <claudio.a.jose@gmail.com>
2022-04-07 15:23:13 +0100

    Add solution for TOP Foundation/ Javascript Basic/ Fundational Part 4 - 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function (str) {
+    let reveredStr = str.split('').reverse().join('');
+    return reveredStr;
 };
 
 // Do not edit below this line
