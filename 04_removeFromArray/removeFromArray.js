800135e41720b0607fca6dc376b84c18db64984b
Claudio Camba <claudio.a.jose@gmail.com>
2022-04-07 17:25:02 +0100

    Add solution for TOP Foundation/ Javascript Basic/ Fundational Part 4 - 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, rmv1, rmv2, rmv3, rmv4) {
+    let removeValues = [rmv1, rmv2, rmv3, rmv4];
+    for (const person of removeValues) {
+        if (arr.indexOf(person) !== -1) {
+            arr.splice(arr.indexOf(person), 1);
+        }
+    }
 
+    return arr;
 };
 
 // Do not edit below this line
