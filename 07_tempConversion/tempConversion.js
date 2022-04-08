b8049e44959a1c117e41a27dac9ffbfd8bc8c2fd
Claudio Camba <claudio.a.jose@gmail.com>
2022-04-08 15:39:08 +0100

    Add solution for TOP Foundation/ Javascript Basic/ Fundational Part 4 - 07_temConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function (f) {
+  f = (f - 32) / 1.8;
+  let fc = parseFloat(f.toFixed(1));
+  return fc;
 };
 
-const ctof = function() {
-
+const ctof = function (c) {
+  c = (c * 1.8) + 32;
+  let cf = parseFloat(c.toFixed(1));
+  return cf;
 };
 
 // Do not edit below this line
