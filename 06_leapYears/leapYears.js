cc05f33435a1d6c252a011d577b2484fbfcc88d4
Claudio Camba <claudio.a.jose@gmail.com>
2022-04-08 12:53:42 +0100

    Add solution for TOP Foundation/ Javascript Basic/ Fundational Part 4 - 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,24 @@
-const leapYears = function() {
+const leapYears = function (year) {
+    // let check4 = false,
+    //     check400 = false;
 
+    // Check if it century number
+    if (year % 100 === 0) {
+        if (year % 400 === 0) {
+            return true;
+        } else {
+            return false;
+        }
+    } else if (year % 4 === 0) {
+        return true;
+    } else {
+        return false;
+    }
+
+
+    // Leap years are years divisible by four
+    // However, years divisible by 100 are not leap years
+    // unless they are divisible by 400
 };
 
 // Do not edit below this line
