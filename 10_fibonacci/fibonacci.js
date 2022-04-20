5ae94e28f171a1008a4d0c7267049b243a04ec4b
Claudio Camba <claudio.a.jose@gmail.com>
2022-04-20 20:08:34 +0100

    TOP Foundation/ Javascript Basic/ Fundational Part 5 - 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+    let total = [];
+    for (var i = 0; i < num; i++) {
+        if (total.length < 2) {
+            total.push(1);
+        } else {
+            total.push(total[total.length - 2] + total[total.length - 1]);
+        }
+    }
+    if (total.length === 0) {
+        return 'OOPS'
+    } else {
+        return total[total.length - 1];
+    }
 };
 
 // Do not edit below this line
