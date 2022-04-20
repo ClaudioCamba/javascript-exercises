78e1b7b2fd25662306d47504bc24ebe09dbd6fdf
Claudio Camba <claudio.a.jose@gmail.com>
2022-04-20 19:30:32 +0100

    TOP Foundation/ Javascript Basic/ Fundational Part 5 - 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,5 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    return str.replace(/[^a-zA-Z]/g, '').toLowerCase() === str.split('').reverse().join('').replace(/[^a-zA-Z]/g, '').toLowerCase();
 };
 
 // Do not edit below this line
