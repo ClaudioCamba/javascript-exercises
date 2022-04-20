2b3b7c98941e1b4f5252aec71409aca9a36f9257
Claudio Camba <claudio.a.jose@gmail.com>
2022-04-20 18:15:11 +0100

    Add solution for TOP Foundation/ Javascript Basic/ Fundational Part 5 - 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,53 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  if (array.length > 0) {
+    return array.reduce((total, num) => total + num);
+  } else {
+    return 0
+  }
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  if (array.length > 0) {
+    return array.reduce((total, num) => total * num);
+  } else {
+    return 0
+  }
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  let total;
+  for (var i = 0; i < b - 1; i++) {
+    if (i === 0) {
+      total = a * a;
+    } else {
+      total *= a;
+    }
+  }
+  return total;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  let total;
+  if (num < 1) {
+    total = 1;
+  } else {
+    for (var i = num; i > 0; i--) {
+      if (i === num) {
+        total = num;
+      } else {
+        total *= i;
+      }
+    }
+  }
+  return total;
 };
 
 // Do not edit below this line
