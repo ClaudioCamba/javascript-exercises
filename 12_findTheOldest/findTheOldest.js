3bdc162dfe39e0726eb1dfea4e000f332f34e9b5
Claudio Camba <claudio.a.jose@gmail.com>
2022-04-20 21:46:17 +0100

    TOP Foundation/ Javascript Basic/ Fundational Part 5 - 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function (obj) {
 
+    let age = 0, persons;
+
+    for (const person of obj) {
+        if ("yearOfDeath" in person) {
+            if ((person.yearOfDeath - person.yearOfBirth) > age) {
+                persons = person;
+                age = person.yearOfDeath - person.yearOfBirth;
+            }
+        } else {
+            if ((new Date().getFullYear() - person.yearOfBirth) > age) {
+                persons = person;
+                age = new Date().getFullYear() - person.yearOfBirth;
+            }
+        }
+    }
+
+    return persons;
 };
 
 // Do not edit below this line
